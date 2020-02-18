8e17142d990d2d2930baa236351adc6544e2d144
雪晴 <zhengjunbo@fcbox.com>
2020-02-18 08:43:04 +0800

    init

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
+	"net/http"
+	"github.com/coolboydan/db-switch/service/config"
+)
+
+var path = flag.String("path", "config.toml", "path")
+
+func main() {
+
+	flag.Parse()
+
+	cfg := config.NewConfig()
+
+	cfg.configFromFile(*path)
+
+	flag.Parse()
+	http.Handle("/metrics", promhttp.Handler())
+
+
+	//开启一个新的线程定时检查线程
+
+
+	//检查是否连接的上,如果连接不上，在3秒内重试3次。
+
+	//检查备用机是否连接的上如果链接的上准备切换，链接不上报错发邮件退出循环。
+
+	//执行切换语句
+
+	//执行完成之后发个邮件
+
+
+	log.Print("service start")
+	log.Fatal(http.ListenAndServe(cfg.Port, nil))
+}
---
+++ service/config.go
@@ -0,0 +1,52 @@
+package service
+
+import (
+	"flag"
+	"github.com/BurntSushi/toml"
+	"github.com/juju/errors"
+	"os"
+)
+
+var (
+	newFile *os.File
+	err     error
+)
+
+type Config struct {
+	*flag.FlagSet `json:"-"`
+
+	Port string `toml:"port" json:"port"`
+
+	NginxPath string `toml:"nginx-path" json:"nginx-path"`
+
+	NginxConfig string `toml:"nginx-config" json:"nginx-config"`
+
+	MonitorDb DBConfig `toml:"monitor-db" json:"monitor-db"`
+
+	SwitchDb DBConfig `toml:"switch-db" json:"switch-db"`
+}
+
+type DBConfig struct {
+	Host string `toml:"host" json:"host"`
+
+	User string `toml:"user" json:"user"`
+
+	Password string `toml:"password" json:"password"`
+
+	Name string `toml:"name" json:"name"`
+
+	Port int `toml:"port" json:"port"`
+}
+
+// NewConfig creates a new config.
+func NewConfig() *Config {
+	cfg := &Config{}
+
+	return cfg
+}
+
+// configFromFile loads config from file.
+func (c *Config) configFromFile(path string) error {
+	_, err := toml.DecodeFile(path, c)
+	return errors.Trace(err)
+}
